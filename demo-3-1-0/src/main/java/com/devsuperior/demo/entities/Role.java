eda86de5da44e278a1732294c93c7745f48e121b
Jonatas <jonatasmateuscontato@gmail.com>
2024-02-14 09:51:16 -0300

    User Role model

---
+++ demo-3-1-0/src/main/java/com/devsuperior/demo/entities/Role.java
@@ -0,0 +1,57 @@
+package com.devsuperior.demo.entities;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_role")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String authority;
+
+    public Role() {
+    }
+
+    public Role(Long id, String authority) {
+        this.id = id;
+        this.authority = authority;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+
+        Role role = (Role) object;
+
+        if (!Objects.equals(id, role.id)) return false;
+        return Objects.equals(authority, role.authority);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (authority != null ? authority.hashCode() : 0);
+        return result;
+    }
+}
